4f8265fa321ae4b960e04c62f56c0654a4067624
BamBoo <64342840+jwk9022648@users.noreply.github.com>
2021-02-25 09:59:26 -0800

    모두 삭제

--- android/app/src/main/kotlin/com/kkumsoft/flutter_jsontest/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.kkumsoft.flutter_jsontest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/model_questionchoice.dart
+++
@@ -1,7 +0,0 @@
-class ModelQuestionChoice {
-  int questionNo;
-  String choiceNo;
-  String choiceScore;
-
-  ModelQuestionChoice({this.questionNo, this.choiceNo, this.choiceScore});
-}
